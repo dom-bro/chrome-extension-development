7742415e30e12009cccacab68a33dd2e7c9fb230
liyunzhao <516911028@qq.com>
2023-12-14 00:05:09 +0800

    Chrome扩展的核心：manifest 文件（下）

---
+++ packages/crx-demo/content-scripts/document_idle_main.js
@@ -0,0 +1,4 @@
+{
+  const  now = new Date()
+  console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${now.getMilliseconds()} %ccontent-script`, 'font-weight: bold;color: #090;', 'document_idle_MAIN', window.customVariable)
+}
